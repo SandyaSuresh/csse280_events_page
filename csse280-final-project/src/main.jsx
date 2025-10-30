782432f080f4dde0f715d09138b9fa400dc65390
SandyaSuresh <sandya.suresh@gmail.com>
2025-10-30 18:06:03 -0400

    database is working

--- csse280-final-project/src/main.jsx
+++ csse280-final-project/src/main.jsx
@@ -2,11 +2,14 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
+import Calendar from './Calendar'
 import NavBar from './NavBar'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <NavBar />
-    {/* <App /> */}
+    <App />
+    <Calendar />
+
   </StrictMode>,
 )
