685f63fc8fc772b2652fa523cff1acdc4012004a
SandyaSuresh <sandya.suresh@gmail.com>
2025-10-30 14:29:18 -0400

    navbar

---
+++ csse280-final-project/src/NavBar.jsx
@@ -0,0 +1,21 @@
+// import { useState } from 'react'
+// import reactLogo from './assets/react.svg'
+// import viteLogo from '/vite.svg'
+import './NavBar.css'
+
+function NavBar() {
+  // const [count, setCount] = useState(0)
+  return (
+    <>
+     <nav className='navbar'>
+      <ul>
+        <li><a href="">For You</a></li>
+        <li><a href="">Explore</a></li>
+        <li><a href = "">Profile</a></li>
+      </ul>
+     </nav>
+     </>
+  );
+};
+
+export default NavBar;
