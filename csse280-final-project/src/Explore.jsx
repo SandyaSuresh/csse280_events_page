812aa86e9da4320db63c0bebfe6c728a9f20e862
Carter Latham <lathamcb@rose-hulman.edu>
2025-11-03 15:07:32 -0500

    explore page things

--- csse280-final-project/src/Explore.jsx
+++ csse280-final-project/src/Explore.jsx
@@ -8,14 +8,14 @@ function Explore() {
     <>
       {/* <div>
         <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
+          <img src={viteLogo} classNameName="logo" alt="Vite logo" />
         </a>
         <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
+          <img src={reactLogo} classNameName="logo react" alt="React logo" />
         </a>
       </div>
       <h1>Vite + React</h1>
-      <div className="card">
+      <div classNameName="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
         </button>
@@ -23,84 +23,84 @@ function Explore() {
           Edit <code>src/App.jsx</code> and save to test HMR
         </p>
       </div>
-      <p className="read-the-docs">
+      <p classNameName="read-the-docs">
         Click on the Vite and React logos to learn more
       </p> */}
-  <div class="top">
+  <div className="top">
     <h1>Explore Page</h1> 
     <input type="text" placeholder="search"></input>
   </div>
   <h2>Popular Today!</h2>
-  <div class="container">
-    <div class="box"></div>
-    <div class="box"></div>
-    <div class="box"></div>
-    <div class="box"></div>
-    <div class="box"></div>
-    <div class="box"></div>
-    <div class="box"></div>
-    <div class="box"></div>
-    <div class="box"></div>
-    <div class="box"></div>
-    <div class="box"></div>
-    <div class="box"></div>
-    <div class="box"></div>
-    <div class="box"></div>
-    <div class="box"></div>
+  <div className="container">
+    <div className="box"></div>
+    <div className="box"></div>
+    <div className="box"></div>
+    <div className="box"></div>
+    <div className="box"></div>
+    <div className="box"></div>
+    <div className="box"></div>
+    <div className="box"></div>
+    <div className="box"></div>
+    <div className="box"></div>
+    <div className="box"></div>
+    <div className="box"></div>
+    <div className="box"></div>
+    <div className="box"></div>
+    <div className="box"></div>
   </div>
   <h2>Sporting Events</h2>
-  <div class="container">
-    <div class="box"></div>
-    <div class="box"></div>
-    <div class="box"></div>
-    <div class="box"></div>
-    <div class="box"></div>
-    <div class="box"></div>
-    <div class="box"></div>
-    <div class="box"></div>
-    <div class="box"></div>
-    <div class="box"></div>
-    <div class="box"></div>
-    <div class="box"></div>
-    <div class="box"></div>
-    <div class="box"></div>
-    <div class="box"></div>
+  <div className="container">
+    <div className="box"></div>
+    <div className="box"></div>
+    <div className="box"></div>
+    <div className="box"></div>
+    <div className="box"></div>
+    <div className="box"></div>
+    <div className="box"></div>
+    <div className="box"></div>
+    <div className="box"></div>
+    <div className="box"></div>
+    <div className="box"></div>
+    <div className="box"></div>
+    <div className="box"></div>
+    <div className="box"></div>
+    <div className="box"></div>
   </div>
   <h2>Multi-Day</h2>
-  <div class="container">
-    <div class="box"></div>
-    <div class="box"></div>
-    <div class="box"></div>
-    <div class="box"></div>
-    <div class="box"></div>
-    <div class="box"></div>
-    <div class="box"></div>
-    <div class="box"></div>
-    <div class="box"></div>
-    <div class="box"></div>
-    <div class="box"></div>
-    <div class="box"></div>
-    <div class="box"></div>
-    <div class="box"></div>
-    <div class="box"></div>
+  <div className="container">
+    <div className="box"></div>
+    <div className="box"></div>
+    <div className="box"></div>
+    <div className="box"></div>
+    <div className="box"></div>
+    <div className="box"></div>
+    <div className="box"></div>
+    <div className="box"></div>
+    <div className="box"></div>
+    <div className="box"></div>
+    <div className="box"></div>
+    <div className="box"></div>
+    <div className="box"></div>
+    <div className="box"></div>
+    <div className="box"></div>
   </div>
   <h2>General Meetings</h2>
-  <div class="container">
-    <div class="box"></div>
-    <div class="box"></div>
-    <div class="box"></div>
-    <div class="box"></div>
-    <div class="box"></div>
-    <div class="box"></div>
-    <div class="box"></div>
-    <div class="box"></div>
-    <div class="box"></div>
-    <div class="box"></div>
-    <div class="box"></div>
-    <div class="box"></div>
-    <div class="box"></div>
-    <div class="box"></div>
-    <div class="box"></div>
+  <div className="container">
+    <div className="box"></div>
+    <div className="box"></div>
+    <div className="box"></div>
+    <div className="box"></div>
+    <div className="box"></div>
+    <div className="box"></div>
+    <div className="box"></div>
+    <div className="box"></div>
+    <div className="box"></div>
+    <div className="box"></div>
+    <div className="box"></div>
+    <div className="box"></div>
+    <div className="box"></div>
+    <div className="box"></div>
+    <div className="box"></div>
   </div>
     </>
   )
