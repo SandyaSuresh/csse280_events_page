538d10520ae356d9218314afa044fbb6554bac99
Carter Latham <lathamcb@rose-hulman.edu>
2025-11-04 13:36:22 -0500

    calendar now shows different months and starts on right day of the week

--- csse280-final-project/src/Calendar.jsx
+++ csse280-final-project/src/Calendar.jsx
@@ -1,9 +1,14 @@
 import { useEffect, useState } from 'react'
 import './Calendar.css'
 
-function createDays(numDays, activeDay, func){
+function createDays(numDays, activeDay, weekdayOfFirst, func){
   let list = document.createElement("ul")
   list.className="days"
+  for (let i = 1; i <= weekdayOfFirst; i++) {
+    let list_item = document.createElement("li");
+    list_item.innerHTML = ""; // creates blank nodes for empty days
+    list.append(list_item);
+  }
   for(let i = 1; i <= numDays; i++){
     let list_item = document.createElement("li");
     list_item.id = i;
@@ -12,54 +17,84 @@ function createDays(numDays, activeDay, func){
     }else{
       list_item.innerHTML = i;
     }
-    list_item.onclick = () => {func(i); createDays(numDays, i, func);}
-    list.appendChild(list_item)
+    list_item.onclick = () => {func(i); createDays(numDays, i, weekdayOfFirst, func);}
+    list.appendChild(list_item);
   }
 
   document.getElementById("days-list").replaceChildren(list)
 }
 
-function Calendar() {
-
-  const [month, setMonth] = useState("")
-  const [day, setDay] = useState(0)
-
-  let dateobj = new Date();
+function loadMonth(monthIndex, year, func) {
   let months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"]
+  let month = months[monthIndex];
+  let weekdayOfFirst = new Date(year, monthIndex, 1).getDay();
+  let numDays  = new Date(year, monthIndex + 1, 0).getDate();
+
+  document.getElementById("current-month").innerHTML = `${month}<br></br>
+            <span>${year}</span>`; // MAYBE JUST {month} IDK
+  createDays(numDays, numDays/*should be active day, this is a placeholder*/, weekdayOfFirst, func);
+          {/* <li>{month}<br></br>
+            <span>2021</span>
+          </li> */}
+
+  //const [month, setMonth] = useState(monthString);
+}
+
+function Calendar() {
+  let dateobj = new Date();
+  // let months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"]
   let monthIndex = dateobj.getMonth();
-  let year = dateobj.getYear();
-  let monthString = months[monthIndex];
+  let year = dateobj.getFullYear();
+  // let monthString = months[monthIndex];
   let days  = new Date(year, monthIndex, 0).getDate();
+
+  // const [month, setMonth] = useState(monthString);
+  const [day, setDay] = useState(days);
+
+  //console.log("monthString: " + monthString);
+  // setMonth(monthString);
+  // setDay(dateobj.getDate());
+
+  // useEffect(() => {createDays(days, day, setDay)}, [day, days]); // replace with loadMonth?????????
   
-  useEffect(() => {createDays(days, day, setDay)}, [day, days]);
+  useEffect(() => {loadMonth(monthIndex, year, setDay)}, [monthIndex, year]);
 
-  window.addEventListener("load", () => {
-    setMonth(monthString);
-    setDay(dateobj.getDate());
-  })
-
-
+  const changeMonth = (change) => {
+    monthIndex += change;
+    if (monthIndex < 0) {
+      monthIndex = 11;
+      year--;
+    } else if (monthIndex > 11) {
+      monthIndex = 0;
+      year++;
+    }
+    loadMonth(monthIndex, year, setDay)
+  }
 
   return (
     <>
-       <div className="month">
+      <div className="month">
         <ul>
-          <li className="prev">Prev</li>
-          <li className="next">Next</li>
-          <li>{month}<br></br>
-            <span>2021</span>
+          <li className="prev" onClick={() => {
+            changeMonth(-1);
+          }}>Prev</li>
+          <li className="next" onClick={() => {
+            changeMonth(1);
+          }}>Next</li>
+          <li id="current-month"><br></br>
+            <span></span>
           </li>
         </ul>
       </div>
 
       <ul className="weekdays">
+        <li>Su</li>
         <li>Mo</li>
         <li>Tu</li>
         <li>We</li>
         <li>Th</li>
         <li>Fr</li>
         <li>Sa</li>
-        <li>Su</li>
       </ul>
       <div id="days-list">
         
